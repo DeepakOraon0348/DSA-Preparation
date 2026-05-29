6f4d5a72ca2b016b6734bf5e8b037cb50936c6f4
Deepak Oraon <deepakoraon0348@gmail.com>
2026-05-29 12:21:41 +0530

    7th commit

--- test.cpp
+++ test.cpp
@@ -1,16 +1,28 @@
 #include<iostream>
 using namespace std;
 int main(){
+    // int n=5;
+    // for(int i=0;i<n;i++){
+    //     for(int k=0;k<=i;k++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+    // for(int i=n-1;i>=1;i--){
+    //     for(int k=0;k<i;k++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
     int n=5;
     for(int i=0;i<n;i++){
-        for(int k=0;k<=i;k++){
-            cout<<"*";
-        }
-        cout<<endl;
-    }
-    for(int i=n-1;i>=1;i--){
-        for(int k=0;k<i;k++){
-            cout<<"*";
+        for(int j=0;j<=i;j++){
+            if((i+j)%2==0){
+                cout<<"1";
+            }
+            else{
+                cout<<"0";
+            }
         }
         cout<<endl;
     }
