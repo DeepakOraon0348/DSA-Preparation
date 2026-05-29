3e146d6d00970d76a02a1566303857f6d7dcb3b7
Deepak Oraon <deepakoraon0348@gmail.com>
2026-05-29 06:58:39 +0530

    6th commit

--- test.cpp
+++ test.cpp
@@ -2,11 +2,14 @@
 using namespace std;
 int main(){
     int n=5;
-    for(int i=n;i>=1;i--){
-        for(int j=0;j<n-i;j++){
-            cout<<" ";
+    for(int i=0;i<n;i++){
+        for(int k=0;k<=i;k++){
+            cout<<"*";
         }
-        for(int k=0;k<i*2-1;k++){
+        cout<<endl;
+    }
+    for(int i=n-1;i>=1;i--){
+        for(int k=0;k<i;k++){
             cout<<"*";
         }
         cout<<endl;
