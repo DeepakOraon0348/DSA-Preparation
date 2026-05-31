ee5f68737ab78a07dd001562ff8823c5e74c579f
Deepak Oraon <deepakoraon0348@gmail.com>
2026-05-31 10:22:03 +0530

    10th commit

--- test.cpp
+++ test.cpp
@@ -87,5 +87,57 @@ int main(){
     //     }
     //     cout<<endl;
     // }
+
+    // int n=5;
+    // for(int i=n;i>=1;i--){
+    //     for(int j=0;j<i;j++){
+    //         cout<<"*";
+    //     }
+    //     for(int k=0;k<(n-i)*2;k++){
+    //         cout<<" ";
+    //     }
+    //     for(int l=0;l<i;l++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<=i;j++){
+    //         cout<<"*";
+    //     }
+    //     for(int k=0;k<(n-i)*2-2;k++){
+    //         cout<<" ";
+    //     }
+    //     for(int l=0;l<=i;l++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    int n=5;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        for(int k=0;k<(n-i)*2-2;k++){
+            cout<<" ";
+        }
+        for(int l=0;l<=i;l++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=n-2;i>=1;i--){
+        for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        for(int k=0;k<(n-i)*2-2;k++){
+            cout<<" ";
+        }
+        for(int l=0;l<=i;l++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
     return 0;
 }
