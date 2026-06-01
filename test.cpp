0a050f49373e7e558d3be3d200a1ae1f92eaa607
Deepak Oraon <deepakoraon0348@gmail.com>
2026-06-01 12:37:55 +0530

    11th commite

--- test.cpp
+++ test.cpp
@@ -127,14 +127,14 @@ int main(){
         }
         cout<<endl;
     }
-    for(int i=n-2;i>=1;i--){
-        for(int j=0;j<=i;j++){
+    for(int i=n-1;i>=1;i--){
+        for(int j=0;j<i;j++){
             cout<<"*";
         }
-        for(int k=0;k<(n-i)*2-2;k++){
+        for(int k=0;k<(n-i)*2;k++){
             cout<<" ";
         }
-        for(int l=0;l<=i;l++){
+        for(int l=0;l<i;l++){
             cout<<"*";
         }
         cout<<endl;
